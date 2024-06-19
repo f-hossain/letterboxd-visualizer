a012fe5059117e9f3baeb715d47029c0d6d4be96
f-hossain <55707559+f-hossain@users.noreply.github.com>
2024-06-19 15:42:08 -0400

    filter styling + cleanup

--- src/components/TreemapJson.jsx
+++ src/components/TreemapJson.jsx
@@ -7,20 +7,15 @@ function TreemapJson() {
     const starFilled = "★"
     const starOutlined = "☆"
 
+    let [data, setData] = useState(top50)
+    let [size, setSize] = useState("likes")
+    let [activeFilters, setActiveFilters] = useState([])
+
     // ==================================================
     //                     FILTERS
     // ==================================================
 
-    let [data, setData] = useState(top50)
-    let [size, setSize] = useState("rating")
-    // let allFilters = top50.children.map( genre => {return genre.name})
-
-    // console.log(allFilters)
-    let [activeFilters, setActiveFilters] = useState([])
-
     let changeSize = (event) => {
-        console.log('changing size')
-        console.log(event.target.value)
         setSize(event.target.value)
     }
 
@@ -29,7 +24,6 @@ function TreemapJson() {
         const genreName = element.getAttribute("data-value")
         const hasSelectedClass = element.classList.contains("btn-selected")
         const isActive = activeFilters.includes(genreName)
-
 
         if (hasSelectedClass || isActive) {
             let i = activeFilters.findIndex( filter => { return filter == genreName })
@@ -46,11 +40,7 @@ function TreemapJson() {
             setActiveFilters(activeFilters)
             element.classList.remove("btn-selected")
         } else {
-            // let filteredData = top50.children.filter( genre => genre.name.includes(genreName))
-            // let newData = { "children": filteredData}
-            // setData(newData)
             let newFilter = [...activeFilters, genreName]
-
             let filteredData = top50.children.filter( genre => newFilter.includes(genre.name))
             let newData = { "children": filteredData}
 
@@ -61,10 +51,14 @@ function TreemapJson() {
     }
 
     // ==================================================
+    //                  RENDER MAP
+    // ==================================================
+
 
     useEffect( () => {
         draw();
     }, [data, size]);
+
 
     // ==================================================
     //                    TREEMAP
@@ -73,8 +67,8 @@ function TreemapJson() {
         d3.select("svg > *").remove()
 
         // set the dimensions and margins of the graph
-        var margin = {top: 10, right: 10, bottom: 10, left: 10},
-        width = 1000 ,
+        var margin = {top: 8, right: 10, bottom: 10, left: 10},
+        width = 1000,
         height = 1000 ;
 
 
@@ -87,6 +81,7 @@ function TreemapJson() {
         .append("g")
         .attr("transform",
             "translate(" + margin.left + "," + margin.top + ")");
+
 
         // ==================================================
         //                    TOOLTIP
@@ -130,8 +125,6 @@ function TreemapJson() {
         // ==================================================
         //                    DATA
         // ==================================================
-        // Give the data to this cluster layout:
-        // Here the size of each leave is given in the 'value' field in input data
         var root = d3.hierarchy(data).sum(function(d){ 
             // TODO: write this better later
             if (size === "fans") { return d.fans}
@@ -168,36 +161,6 @@ function TreemapJson() {
             .on("mouseleave", mouseleave)
             .on("mouseover", mouseover)
         
-
-        svg.selectAll("text").data(root.leaves()).enter().append('text')
-            .attr('opacity', function(d) {
-                const width = (d.x1) - (d.x0), height = (d.y1) - (d.y0);
-                const tooSmall = width < 60 || height < 50
-                const opacity = tooSmall ? 0 : 0.9
-                return opacity
-            })
-            .attr("x", function(d){
-                const width = d.x1 - d.x0, height = d.y1 - d.y0;
-                const gap_size = Math.max(Math.min(width/10, height/4, Math.sqrt((width*width + height*height))/10), 9) / 2
-                return d.x0 + gap_size // + gap size to adjust position (more right)
-            })    
-            .attr("y", function(d){ 
-                const width = d.x1 - d.x0, height = d.y1 - d.y0;
-                const gap_size = Math.max(Math.min(width/12, height/4, Math.sqrt((width*width + height*height))/10), 9) * 1.5
-                return d.y0 + gap_size // + gap size to adjust position (lower)
-            })
-            // only show text if the card is big enough to display it properly
-            .text(function(d){ return d.data.title + " " + d.data.year})
-            .attr("font-size", function(d) {
-                const width = d.x1 - d.x0, height = d.y1 - d.y0;
-                return Math.max(Math.min(width/10, height/5, Math.sqrt((width*width + height*height))/10), 9)
-            })
-            .attr("fill", "grey")
-            .call(wrapText)
-            .on("mousemove", mousemove)
-            .on("mouseleave", mouseleave)
-            .on("mouseover", mouseover)
-        
         svg.selectAll("text").data(root.leaves()).enter().append('text')
         .attr('opacity', function(d) {
             const width = (d.x1) - (d.x0), height = (d.y1) - (d.y0);
@@ -205,8 +168,27 @@ function TreemapJson() {
             const opacity = tooSmall ? 0 : 0.9
             return opacity
         })
-        .text(function(d){ return d.data.year})
-
+        .attr("x", function(d){
+            const width = d.x1 - d.x0, height = d.y1 - d.y0;
+            const gap_size = Math.max(Math.min(width/10, height/4, Math.sqrt((width*width + height*height))/10), 9) / 2
+            return d.x0 + gap_size // + gap size to adjust position (more right)
+        })    
+        .attr("y", function(d){ 
+            const width = d.x1 - d.x0, height = d.y1 - d.y0;
+            const gap_size = Math.max(Math.min(width/12, height/4, Math.sqrt((width*width + height*height))/10), 9) * 1.5
+            return d.y0 + gap_size // + gap size to adjust position (lower)
+        })
+        // only show text if the card is big enough to display it properly
+        .text(function(d){ return d.data.title + " " + d.data.year})
+        .attr("font-size", function(d) {
+            const width = d.x1 - d.x0, height = d.y1 - d.y0;
+            return Math.max(Math.min(width/10, height/5, Math.sqrt((width*width + height*height))/10), 9)
+        })
+        .attr("fill", "grey")
+        .call(wrapText)
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave)
+        .on("mouseover", mouseover)
     }
 
     // ==================================================
@@ -220,9 +202,7 @@ function TreemapJson() {
             const height = nodeData.y1 - nodeData.y0
 
             let fontSizeLine = Math.max(Math.min(width/12, height/4, Math.sqrt((width*width + height*height))/10), 9)
-
             const lineLimit = width - fontSizeLine
-
 
             let word;
             const words = node.text().split(' ').reverse();
@@ -248,8 +228,6 @@ function TreemapJson() {
                 tspan = addTspan(word);
               }
             }
-
-            // words.pop()
             
             addTspan(words.pop(), true);
         
@@ -264,7 +242,6 @@ function TreemapJson() {
                       .attr('y', y)
                       .attr('dy', `${lineNumber * fontSizeLine}px`)
                       .attr('font-size', function(d) { return fontSizeLine / 2})
-                    //   .attr('fill', 'red')
                       .classed("year", true)
                       .text(nodeData.data.director + ", " + text)
                   );
@@ -277,38 +254,39 @@ function TreemapJson() {
                       .attr('y', y)
                       .attr('dy', `${lineNumber * fontSizeLine}px`)
                       .text(text)
-                  );
-
+                );
               }
-              
             }
         });
-    
     }
 
 
     return (
         <div>
             <div className="filter-inputs">
-                <label>calculate by: </label>
-                <select value={size} onChange={changeSize} >
-                    <option value="rating">rating</option>
-                    <option value="likes">likes</option>
-                    <option value="fans">fans</option>
-                    <option value="watches">watches</option>
-                </select>
-                <label> genres: </label>
-                <div className="genres">
-                    <button data-value="action" onClick={updateGenreFilter}>action</button>
-                    <button data-value="crime" onClick={updateGenreFilter}>crime</button>
-                    <button data-value="drama" onClick={updateGenreFilter}>drama</button>
-                    <button data-value="fantasy" onClick={updateGenreFilter}>fantasy</button>
-                    <button data-value="history" onClick={updateGenreFilter}>history</button>
-                    <button data-value="romance" onClick={updateGenreFilter}>romance</button>
-                    <button data-value="science fiction" onClick={updateGenreFilter}>science fiction</button>
-                    <button data-value="thriller" onClick={updateGenreFilter}>thriller</button>
-                    <button data-value="war" onClick={updateGenreFilter}>war</button>
-                    <button data-value="western" onClick={updateGenreFilter}>western</button>
+                <div className="select-container">
+                    <label>select tile size: </label>
+                    <select value={size} onChange={changeSize} >
+                        <option value="rating">rating</option>
+                        <option value="likes">likes</option>
+                        <option value="fans">fans</option>
+                        <option value="watches">watches</option>
+                    </select>
+                </div>
+                <div className="buttons-container">
+                    <label> browse by genre: </label>
+                    <div className="genres">
+                        <button data-value="action" className="action" onClick={updateGenreFilter}>action</button>
+                        <button data-value="crime" className="crime" onClick={updateGenreFilter}>crime</button>
+                        <button data-value="drama" className="drama" onClick={updateGenreFilter}>drama</button>
+                        <button data-value="fantasy" className="fantasy" onClick={updateGenreFilter}>fantasy</button>
+                        <button data-value="history" className="history" onClick={updateGenreFilter}>history</button>
+                        <button data-value="romance" className="romance" onClick={updateGenreFilter}>romance</button>
+                        <button data-value="science fiction" className="science-fiction" onClick={updateGenreFilter}>science fiction</button>
+                        <button data-value="thriller" className="thriller" onClick={updateGenreFilter}>thriller</button>
+                        <button data-value="war" className="war" onClick={updateGenreFilter}>war</button>
+                        <button data-value="western" className="western" onClick={updateGenreFilter}>western</button>
+                    </div>
                 </div>
             </div>
             <div className="svg-container" width="1000" ref={ref}></div>
